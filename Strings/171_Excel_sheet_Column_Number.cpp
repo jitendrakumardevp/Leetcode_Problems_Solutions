b4bc8f73ba046d237de2ddb11603b1cf50129e28
Jitendra Kumar <99542823+jitendrakumardevp@users.noreply.github.com>
2026-05-30 21:02:13 +0530

    Implement titleToNumber function for Excel columns

---
+++ Strings/171_Excel_sheet_Column_Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+
+        int col_Number = 0;
+        int strSize = columnTitle.size();
+
+        for (int i = 0; i < strSize; i++) {
+            char ch = columnTitle[i];
+            int currVal = ch - 'A' + 1;
+            col_Number = col_Number * 26 + currVal;
+        }
+
+        return col_Number;
+    }
+};
